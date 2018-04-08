1414cf4a13fe107c9b2ed7eb4d214ed33f3a8859
sankalpchauhan-me <sankalpchauhan.me@gmail.com>
2018-04-08 14:57:08 +0530

    Initial commit

---
+++ app/src/main/java/com/ipu/techtalk/studio/SimpleFragmentPagerAdapter.java
@@ -0,0 +1,42 @@
+package com.ipu.techtalk.studio;
+
+/**
+ * Created by sankalp on 8/4/18.
+ */
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Provides the appropriate {@link Fragment} for a view pager.
+ */
+public class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
+    private String tabTitles[] = new String[] { "Tab1", "Tab2", "Tab3" };
+
+    public SimpleFragmentPagerAdapter(FragmentManager fm, MainActivity mainActivity) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new FirstFragment();
+        } else if (position == 1){
+            return new SecondFragment();
+        } else{
+            return new ThirdFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        return tabTitles[position];
+    }
+}
