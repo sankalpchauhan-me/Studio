bc56a9766b740d312def604bb4cae1273418caa2
Sankalp Chauhan <sankalpchauhan.me@gmail.com>
2018-04-22 00:27:08 +0530

    Reduced Splash Screen Time in SPLASH_TIME_OUT
    Earlier 3 seconds now 2 Seconds

--- app/src/main/java/com/ipu/techtalk/studio/Splash.java
+++ app/src/main/java/com/ipu/techtalk/studio/Splash.java
@@ -6,7 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Splash extends AppCompatActivity {
-public static int SPLASH_TIME_OUT = 3000;
+public static int SPLASH_TIME_OUT = 2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
